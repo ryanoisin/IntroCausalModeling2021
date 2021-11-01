37fea82dd18e96614d75f49f43f38c5a27c807a2
Oisin Ryan <ryanoisin@gmail.com>
2021-11-01 08:16:21 +0100

    added setup file

---
+++ setup.R
@@ -0,0 +1,10 @@
+ # Package for Part 1: Potential Outcomes
+
+ install.packages("tableone")
+ install.packages("MatchIt")
+ install.packages("survey")
+ 
+ # Packages for Part 2: Structural Causal Models
+ install.packages("tidyverse")  # For dplyr, ggplot, and friends
+ install.packages("ggdag")      # For plotting DAGs
+ install.packages("dagitty")    # For working with DAG logic
